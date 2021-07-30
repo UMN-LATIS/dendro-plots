7a45dbfe73e1c6900c38b57b88eee8089b324472
thorn573 <thorn573@umn.edu>
2021-07-30 17:26:14 -0500

    Template setup.

--- plotting-app/src/App.vue
+++ plotting-app/src/App.vue
@@ -1,26 +1,81 @@
 <template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+  <div id="management-wrapper">
+    <div id="file-mangement">
+      <h1> File Management </h1>
+    </div>
+    <div id="time-series-management">
+      <h1> Time Series Management</h1>
+    </div>
+  </div>
+
+  <div id="plot-wrapper">
+    <div id="plotly-width">
+      <h1> Plotly Width Plot</h1>
+    </div>
+    <div id="plotly-index">
+      <h1> Plotly Index Plot</h1>
+    </div>
+  </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import $ from 'jquery'
+import 'jquery-ui'
+import simpleSmoothingSpline from 'simple-smoothing-spline'
+import Plotly from 'plotly.js-dist'
 
 export default {
   name: 'App',
-  components: {
-    HelloWorld
-  }
+  components: {},
 }
 </script>
 
 <style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+  #app, body, html {
+    margin: 0;
+    height: 100%;
+    width: 100%;
+  }
+
+  div {
+    display: inline-block;
+  }
+
+  h1 {
+    color: white;
+  }
+
+  #management-wrapper {
+    width: 30%;
+    height: 100%;
+  }
+
+  #file-mangement {
+    background: #ff0000;
+    width: 100%;
+    height: 50%;
+  }
+
+  #time-series-management {
+    background: #0040ff;
+    width: 100%;
+    height: 50%;
+  }
+
+  #plot-wrapper {
+    width: 70%;
+    height: 100%;
+  }
+
+  #plotly-width {
+    background: #bb00ff;
+    width: 100%;
+    height: 50%;
+  }
+
+  #plotly-index {
+    background: #02d65d;
+    width: 100%;
+    height: 50%;
+  }
 </style>
