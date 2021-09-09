61f51700bd15d3b86fbd34ffe8380b68bd35f96e
James Johnson <johnsojr@umn.edu>
2021-09-09 14:35:48 -0500

    install @umn-latis version

--- src/modules/spline.js
+++ src/modules/spline.js
@@ -1,46 +1,46 @@
-import simpleSmoothingSpline from 'simple-smoothing-spline'
-import store from './store.js'
+import simpleSmoothingSpline from "@umn-latis/simple-smoothing-spline";
+import store from "./store.js";
 
 const createSpline = function(x, y, freq, id, propA, propB) {
-  console.log('created spline')
+  console.log("created spline");
 
   let data = x.map((e, i) => {
     let pair = {
       x: e,
-      y: y[i]
-    }
-    return pair
-  })
+      y: y[i],
+    };
+    return pair;
+  });
 
-  let lambda = 0.00001 * Math.pow(2, 9.9784 * Math.log(freq) + 3.975)
-  const spline = simpleSmoothingSpline(data, { 'lambda': lambda })
+  let lambda = 0.00001 * Math.pow(2, 9.9784 * Math.log(freq) + 3.975);
+  const spline = simpleSmoothingSpline(data, { lambda: lambda });
 
-  let xArr = []
-  let yArr = []
+  let xArr = [];
+  let yArr = [];
   for (const obj of spline.points) {
-    xArr.push(obj.x)
-    yArr.push(obj.y)
+    xArr.push(obj.x);
+    yArr.push(obj.y);
   }
 
   // save spline to spline cache in store.js
-  let splineObj
-  let splineExists = store.cache.splines[propA].find(o => o.id == id)
+  let splineObj;
+  let splineExists = store.cache.splines[propA].find((o) => o.id == id);
   if (splineExists) {
-    splineObj = splineExists
+    splineObj = splineExists;
   } else {
-    splineObj = new Object()
-    splineObj.id = id
+    splineObj = new Object();
+    splineObj.id = id;
     // if calculating spline for index, save spline in in raw attribute not index attribute
-    let prop = (propB) ? propB : propA
-    store.cache.splines[prop].push(splineObj)
+    let prop = propB ? propB : propA;
+    store.cache.splines[prop].push(splineObj);
   }
   splineObj[freq] = {
     x: xArr,
     y: yArr,
     eq: spline,
-  }
+  };
 
-  return { x: xArr, y: yArr, eq: spline }
-}
+  return { x: xArr, y: yArr, eq: spline };
+};
 
-export default createSpline
+export default createSpline;
