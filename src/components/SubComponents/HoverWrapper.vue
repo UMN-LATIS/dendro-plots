5455ee709675f7902c544afe1f02fbed7775f37d
thorn573 <thorn573@umn.edu>
2021-12-29 17:11:12 -0600

    Generalized hover text wrapper. Need to extend to other hover text instances.

---
+++ src/components/SubComponents/HoverWrapper.vue
@@ -0,0 +1,70 @@
+<template>
+  <div class="dropdown"
+       :style="{ 'width': isWidth + 'px' }"
+  >
+
+    <component :is="isComponent" v-bind="isData" />
+
+    <div class="dropdown-content"
+         :style="{ 'margin-left': isMarginLeft + 'px' }"
+    >
+      <p class="info"> {{ info }} </p>
+    </div>
+
+  </div>
+</template>
+
+<script>
+  import Toggle from './Toggle.vue'
+  import Dropdown from './Dropdown.vue'
+  import ColorSwatch from './ColorSwatch.vue'
+
+  export default {
+    inject: ['store'],
+    props: ['isMarginLeft', 'isWidth', 'isData', 'isComponent', 'info'],
+    components: { Toggle, Dropdown, ColorSwatch },
+  }
+</script>
+
+<style scoped>
+  .info {
+    margin: 0;
+    padding: 0;
+    font-weight: normal;
+    font-family: sans-serif;
+    font-size: 12px;
+    color: black;
+    width: max-content;
+  }
+
+  .dropdown {
+    height: 20px;
+    display: inline-block;
+    border: none;
+  }
+
+  .dropdown-content {
+    visibility: hidden;
+    opacity: 0;
+    transition: opacity 0s;
+    transition-delay: 1s;
+    font-weight: normal;
+    font-family: Sans-serif;
+    font-size: 12px;
+    color: black;
+    padding: 6px;
+    margin: 0;
+    position: absolute;
+    margin-top: 20px;
+    background-color: #f6f6f6;
+    border: 1px solid black;
+    border-radius: 2px;
+    z-index: 999999;
+  }
+
+  .dropdown:hover .dropdown-content {
+    visibility: visible;
+    opacity: 1;
+    display: inline;
+  }
+</style>
--- src/components/SubComponents/Modal.vue
+++ src/components/SubComponents/Modal.vue
@@ -3,76 +3,27 @@
        :style=" { top: top + 'px' } "
   >
 
-    <Info :info="info"
-          :width="20"
-          :height="20"
+    <HoverWrapper v-for="(data, i) in modalTools" :key="i"
+                  :isMarginLeft="data.left"
+                  :isWidth="data.width"
+                  :isData="data"
+                  :isComponent="data.component"
+                  :info="data.info"
     />
-
-    <!-- ALL SPACER DIVS DECIDED BY WIDTH OF SELECT TAG WIDTH IN DEV TOOLS (+5 wiggle room)-->
-
-    <!-- data options -->
-    <Toggle :id="id"
-            :toggleProp="'rawPointsActive'"
-    />
-    <Dropdown :id="id"
-              :options="splineOptions"
-              :optionModifer="'detrending spline'"
-              :dropdownProp="'rawSplineFreq'"
-    />
-    <div style="width: 152px"></div> <!-- spacer div -->
-    <Dropdown :id="id"
-              :options="store.cache.plots"
-              :optionModifer="': raw data'"
-              :dropdownProp="'rawPlotLocation'"
-     />
-
-    <!-- index options -->
-    <div style="width: 114px"></div> <!-- spacer div -->
-    <Dropdown :id="id"
-              :options="splineOptions"
-              :optionModifer="'index points'"
-              :dropdownProp="'indexPointsFreq'"
-    />
-    <div style="width: 125px"></div> <!-- spacer div -->
-    <Dropdown :id="id"
-              :options="splineOptions"
-              :optionModifer="'visualization spline'"
-              :dropdownProp="'indexSplineFreq'"
-    />
-    <div style="width: 161px"></div> <!-- spacer div -->
-    <Dropdown :id="id"
-              :options="store.cache.plots"
-              :optionModifer="': index data'"
-              :dropdownProp="'indexPlotLocation'"
-     />
-
-     <!-- color options -->
-     <div style="width: 124px"></div> <!-- spacer div -->
-     <ColorSwatch :id="id" />
-     <div :style="{ width: toggleWidth + 'px' }"></div> <!-- spacer div -->
-     <Toggle :id="id"
-             :toggleProp="'applyColorToRaw'"
-     />
-     <!-- marker options -->
-     <Dropdown :id="id"
-               :options="store.cache.shapes"
-               :optionModifer="''"
-               :dropdownProp="'shape'"
-      />
 
   </div>
 </template>
 
 <script>
-  import Info from './Info.vue'
   import Toggle from './Toggle.vue'
   import Dropdown from './Dropdown.vue'
   import ColorSwatch from './ColorSwatch.vue'
+  import HoverWrapper from './HoverWrapper.vue'
 
   export default {
     inject: ['store'],
     props: ['id', 'top'],
-    components: { Info, Toggle, Dropdown, ColorSwatch },
+    components: { Toggle, Dropdown, ColorSwatch, HoverWrapper },
     data: function() {
       return {
         info: ['From left to right, options include: ',
@@ -97,10 +48,98 @@
                           { value: 0.5, name: '50% ' },
                           { value: 0.67, name: '67% ' },
                         ],
-        dropdownWidth: 150,
-        toggleWidth: 16,
       }
     },
+    computed: {
+      modalTools: function() {
+        // all widths decided by inspecting element in dev tools (+5 for wiggle room)
+        return [
+          {
+            id: this.id,
+            left: -20,
+            width: 20,
+            component: 'Toggle',
+            info: 'Toggle raw data appearance in plot.',
+            toggleProp: 'rawPointsActive',
+          }, // raw points toggle
+          {
+            id: this.id,
+            left: 0,
+            width: 152,
+            component: 'Dropdown',
+            info: 'Select visualization spline wavelength for raw data.',
+            options: this.splineOptions,
+            optionModifer: 'detrending spline',
+            dropdownProp: 'rawSplineFreq',
+          }, // raw spline dropdown
+          {
+            id: this.id,
+            left: 0,
+            width: 114,
+            component: 'Dropdown',
+            info: 'Select plot for raw data appearance.',
+            options: this.store.cache.plots,
+            optionModifer: ': raw data',
+            dropdownProp: 'rawPlotLocation',
+          }, // raw plot dropdown
+          {
+            id: this.id,
+            left: 0,
+            width: 125,
+            component: 'Dropdown',
+            info: 'Select detrending spline wavelength for indexing raw data.',
+            options: this.splineOptions,
+            optionModifer: 'index points',
+            dropdownProp: 'indexPointsFreq',
+          }, // index points dropdown
+          {
+            id: this.id,
+            left: 0,
+            width: 161,
+            component: 'Dropdown',
+            info: 'Select visualization spline wavelength for indexed data.',
+            options: this.splineOptions,
+            optionModifer: 'visualization points',
+            dropdownProp: 'indexSplineFreq',
+          }, // index spline dropdown
+          {
+            id: this.id,
+            left: 0,
+            width: 124,
+            component: 'Dropdown',
+            info: 'Select plot for indexed data appearance.',
+            options: this.store.cache.plots,
+            optionModifer: ': index data',
+            dropdownProp: 'indexPlotLocation',
+          }, // index plot dropdown
+          {
+            id: this.id,
+            left: 0,
+            width: 16,
+            component: 'ColorSwatch',
+            info: 'Select color for this time series.',
+          }, // color
+          {
+            id: this.id,
+            left: -20,
+            width: 20,
+            component: 'Toggle',
+            info: 'Toggle color/gray between time series and spline..',
+            toggleProp: 'applyColorToRaw',
+          }, // color toggle
+          {
+            id: this.id,
+            left: 0,
+            width: 124,
+            component: 'Dropdown',
+            info: 'Select shape of marker for data values.',
+            options: this.store.cache.shapes,
+            optionModifer: '',
+            dropdownProp: 'shape',
+          }, // shapes dropdown
+        ]
+      }
+    }
   }
 </script>
 
@@ -120,6 +159,6 @@
     border-left: none;
     border-top-right-radius: 4px;
     border-bottom-right-radius: 4px;
-    width: 900px;
+    width: 878px;
   }
 </style>
